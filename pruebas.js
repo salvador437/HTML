09ec868ffa3909a59d4df7ebef38e51a23285086
salvador437 <telacojo@salva.com>
2024-08-29 10:53:53 +0200

    primer comit repositorio HTML master

---
+++ pruebas.js
@@ -0,0 +1,7 @@
+document.write('a = ' + a + ', b = ' + b + '<P>');
+document.write('a + b = ' + (a + b) + '<BR>');
+document.write('a - b = ' + (a - b) + '<BR>');
+document.write('a * b = ' + (a * b) + '<BR>');
+document.write('a / b = ' + (a / b) + '<BR>');
+document.write('a % b = ' + (a % b) + '<BR>');
+document.write('a++ : ' + (a++) + '<P>');
