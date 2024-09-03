59be0565d7f854647078e6da22e38f31a9d418c1
salvador437 <telacojo@salva.com>
2024-09-03 12:21:41 +0200

    actualizado archivo pruebas-curso.js

--- pruebas-cuso.js
+++ pruebas-cuso.js
@@ -9,15 +9,15 @@ console.log("¡Hola a todos! Observen este número: ", 5 + 18);
 console.debug("Hola Mundo", [1, 2, 3]);
 // 	Muestra mensajes de información.
 console.info("nivel correcto");
-//Muestra información de advertencia. Aparece destacado en amarillo.
+//Muestra información de advertencia. Aparece destacado en amarillo.OK
 console.warn("Precaución");
-//Muestra información de error. Aparece destacado en rojo.
+//Muestra información de error. Aparece destacado en rojo. OK
 console.error("Elemento no definido");
 
-//Mostrar una traza de error
+//Mostrar una traza de error   OK
 console.trace("¡He llegado aquí!");
 
-//Mensaje con estilo CSS
+//Mensaje con estilo CSS   OK
 const styles = `
   background:linear-gradient(#884ced, #ec1cce);
   color:#fff;
@@ -28,11 +28,59 @@ console.log("%c¡Estudia y practica", styles);
 console.log("%c otra prueba ", `background-color:red;color:white;padding:10px;border-radius:9px;font-size:18px`);
 
 
-//Limpia la consola. Equivalente a escribir clear().
+//Limpia la consola. Equivalente a escribir clear().  OK
 //console.clear();
 
 //Agrupar mensajes.
+//En la consola nos aparecera un ► Información a mostrar   OK
 console.group("Información a mostrar");
 console.log("UA: ", navigator.userAgent);
 console.log("Lang: ", navigator.language);
 console.groupEnd();
+
+//El método console.table() OK
+const users = [
+  { name: "Manz", role: "streamer", status: "happy" },
+  { name: "BlurSoul_", role: "mod", status: "happy" },
+  { name: "felixicaza", role: "mod", status: "happy" },
+  { name: "pheralb", role: "mod", status: "porosad" },
+];
+
+console.table(users);
+
+//El método console.assert() OK
+console.assert(5 < 10, "5 es menor que 10");
+// No ocurre nada
+console.assert(5 < 0, "5 es menor que 0");
+// Muestra el mensaje indicado con un aviso de error
+
+
+//El método console.dir() OK
+console.log(document.body);
+console.dir(document.body);
+
+//Crear benchmarks rápidos (NOTA:en firefox no funciona)
+//Contadores
+for (let i = 0; i < 5; i++) {
+  console.count("test-loop");
+}
+console.countEnd("test-loop");
+
+
+//Test de velocidad  (OK en consola navegador)
+console.time("test1");
+for (let i = 0; i < 10; i++) {
+ 
+  console.timeLog("test1", i);
+}
+console.timeEnd("test1");
+
+
+//Test de rendimiento   (no me funciona en firefox)
+console.profile();
+for (let i = 0; i < 10; i++) {
+  console.log("test1", i);
+}
+console.profileEnd();
+
+
